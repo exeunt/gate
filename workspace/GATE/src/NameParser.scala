9aabbdd7c43f45a640a69d41fcfe9fe5bbb22526
slee <slee@C2JPJF1.morningstar.com>
2010-07-12 17:03:07 -0500

    s

--- workspace/GATE/src/NameParser.scala
+++ workspace/GATE/src/NameParser.scala
@@ -9,22 +9,12 @@ import _root_.gate.util._
 
 object NameParser {
 	def main(args: Array[String]): Unit = {
-		println("Weee")
 		Gate.init()
-
-	    // Load ANNIE plugin
 	    val gateHome = Gate.getGateHome()
-	    val pluginsHome = new File(gateHome, "plugins")
-	    Gate.getCreoleRegister().registerDirectories(new File(pluginsHome, "ANNIE").toURL())
 	
-
-	    // create a GATE corpus and add a document for each command-line
-	    // argument
 	    var corpus: Corpus = Factory.createResource("gate.corpora.CorpusImpl").asInstanceOf[Corpus]
-		
-		val docFile = new File("""C:\My Dropbox\GATE\Datastore\gate.corpora.DocumentImpl""")
+		val docFile = new File("""C:\My Dropbox\GATE\Data Samples\Manager Section""")
 		corpus.populate(docFile.toURL(), null, "utf-8", false)
 		println(corpus.getDocumentNames())
 	}
-
 }
